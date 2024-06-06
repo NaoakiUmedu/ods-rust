11a8197a1fb3d075deef3dca6d171af05d39aa7e
Naoaki Umezu <n.umezu1014@gmail.com>
2024-06-06 23:49:40 +0900

    1章 T1.1 3を実施

--- src/chapter1/mod.rs
+++ src/chapter1/mod.rs
@@ -1,5 +1,7 @@
+use std::collections::VecDeque;
+
 /// 入力を1行づつ読み、その逆順で出力せよ。
-fn t1(input: Vec<&str>) -> Vec<&str> {
+fn t1_1(input: Vec<&str>) -> Vec<&str> {
     let mut rev = input.clone();
     rev.reverse();
     rev
@@ -9,7 +11,7 @@ fn t1(input: Vec<&str>) -> Vec<&str> {
 /// その後、続く50行を読み、それを逆順で出力せよ。
 /// これを読み取る行がなくなるまで繰り返し、
 /// 最後に残った行（50行未満かもしれない）もやはり逆順で出力せよ。
-fn t2(input: Vec<String>) -> Vec<String> {
+fn t1_2(input: Vec<String>) -> Vec<String> {
     let mut result: Vec<String> = Vec::new();
     let mut buf: Vec<String> = Vec::new();
     let mut cnt = 0;
@@ -31,11 +33,29 @@ fn t2(input: Vec<String>) -> Vec<String> {
     if cnt <= 50 {
         buf.reverse();
         result.append(&mut buf.clone());
-        buf = Vec::new();
     } else {
         result.append(&mut buf.clone());
-        buf = Vec::new();
-        cnt = 0;
+    }
+    result
+}
+
+/// 入力を1行ずつ読み取り、42行め以降で空行を見つけたら、その42行前の行を出力せよ。
+/// 例えば、242行めが空行であれば200行めを出力せよ。
+/// なお、プログラムの実行中に43行以上の行を保持してはならない。
+fn t1_3(input: Vec<String>) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+    let mut buff: VecDeque<String> = VecDeque::new();
+    for now in input {
+        if buff.len() >= 42 {
+            let previous42 = buff.pop_front();
+            if now.is_empty() {
+                match previous42 {
+                    Some(s) => result.push(s),
+                    None => (),
+                }
+            }
+        }
+        buff.push_back(now);
     }
     result
 }
@@ -45,7 +65,7 @@ mod tests {
     use crate::chapter1;
 
     #[test]
-    fn t1_1() {
+    fn t1_1_1() {
         let input = vec![
             "春過ぎて",
             "夏来にけらし",
@@ -62,11 +82,11 @@ mod tests {
             "春過ぎて",
         ];
 
-        assert_eq!(expect, chapter1::t1(input));
+        assert_eq!(expect, chapter1::t1_1(input));
     }
 
     #[test]
-    fn t2_1() {
+    fn t1_2_1() {
         let input: Vec<String> = (0..120).map(|x| i32::to_string(&x)).collect();
         let expect_num: Vec<i32> = vec![
             49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28,
@@ -78,6 +98,17 @@ mod tests {
         ];
         let expect: Vec<String> = expect_num.iter().map(|x| i32::to_string(&x)).collect();
 
-        assert_eq!(expect, chapter1::t2(input));
+        assert_eq!(expect, chapter1::t1_2(input));
+    }
+
+    #[test]
+    fn t1_3_1() {
+        let mut input: Vec<String> = (0..242).map(|x| i32::to_string(&x)).collect();
+        input[41] = String::from("");
+        input[141] = String::from("");
+        input[241] = String::from("");
+        let expect: Vec<String> = vec!["99".to_string(), "199".to_string()];
+
+        assert_eq!(expect, chapter1::t1_3(input));
     }
 }
